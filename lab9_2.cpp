19991a6cd22d378512ed2457e911b5d04105ee93
Athikan <athikan_thoyaem@cmu.ac.th>
2024-12-13 21:41:38 +0700

    "lab9_2Complete"

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -2,9 +2,23 @@
 using namespace std;
 
 //Write the function printO() here
-
+	void printO(int N,  int M ){
+		cin >> N ;
+		cin >> M ;
+if(N <=0 || M <= 0){
+	cout << "Invalid input" ;
+}else{
+	for(int i = 0 ; i < N ; i++) {
+		for(int j = 0 ; j < M ; j++){
+			cout << "O" ; 
+		}
+		cout << "\n";
+	   }
+	}
+	}
 int main(){
-	
+
+
 	printO(2,2);
 	cout << "\n";
 	
